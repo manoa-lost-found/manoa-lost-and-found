d377de1a77eb99c03d0ebf128c77b332f542333c
Jermaine Bruno <jbruno7@hawaii.edu>
2025-11-19 08:26:13 -1000

    Leads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       </p>
 
       <a
-        href="/auth/uh-login"
+        href="https://authn.hawaii.edu/cas/login?service=https://manoa-lost-and-found.vercel.app"
         className="btn btn-success btn-lg mt-3 px-4 py-2"
         style={{ backgroundColor: '#024731', borderColor: '#024731' }}
       >
