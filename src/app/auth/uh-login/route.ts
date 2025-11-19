7f738e66b4025a2757a222057616595773a80a0b
Jermaine Bruno <jbruno7@hawaii.edu>
2025-11-19 08:13:32 -1000

    Fix syntax error in GET function

--- src/app/auth/uh-login/route.ts
+++ src/app/auth/uh-login/route.ts
@@ -1,6 +1,6 @@
 export default function GET() {
   const loginUrl = `https://authn.hawaii.edu/cas/login?service=${encodeURIComponent(
-    process.env.NEXT_PUBLIC_UH_CALLBACK_URL || ''
+    process.env.NEXT_PUBLIC_UH_CALLBACK_URL || '',
   )}`;
 
   return Response.redirect(loginUrl);
