5c8e726935d18656d09976445caca3f168f73ea2
Edward Uzueta <uzueta@hawaii.edu>
2025-12-03 19:30:23 -1000

    Update route.ts

--- src/app/api/admin/users/route.ts
+++ src/app/api/admin/users/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
-async function GET() {
+export async function GET() {
   try {
     const users = await prisma.user.findMany({
       include: {
@@ -26,5 +26,3 @@ async function GET() {
     );
   }
 }
-
-export default GET;
