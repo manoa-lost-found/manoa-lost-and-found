5d9a53f66b26fdcba3f5ba64ac29b16137d712f1
Edward Uzueta <uzueta@hawaii.edu>
2025-12-09 16:44:51 -1000

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,17 @@
+/* eslint-disable import/prefer-default-export */
 import { withAuth } from 'next-auth/middleware';
 import { NextResponse } from 'next/server';
 
 export default withAuth(
   function middleware(req) {
-    const role = req.nextauth.token?.role;
+    const role = req.nextauth.token?.randomKey;
 
+    // Auto-logout if DISABLED
     if (role === 'DISABLED') {
       const url = new URL('/auth/signout', req.url);
       const res = NextResponse.redirect(url);
 
+      // Clear session token immediately
       res.cookies.set('__Secure-next-auth.session-token', '', {
         maxAge: 0,
         path: '/',
@@ -24,14 +27,16 @@ export default withAuth(
       authorized: ({ token }) => {
         if (!token) return false;
 
-        if (token.role === 'DISABLED') return false;
+        // Block DISABLED users
+        if (token.randomKey === 'DISABLED') return false;
 
         return true;
       },
     },
-  }
+  },
 );
 
+// IMPORTANT — DO NOT run middleware on ANY API routes or admin actions
 export const config = {
   matcher: [
     '/((?!api|auth|admin/user-actions|_next|favicon.ico).*)',
